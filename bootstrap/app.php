230c855d332eb8d0f58e448fb46e9524a3eae61d
Nafiur Rahman <nrsajib@gmail.com>
2024-08-26 11:15:11 +0600

    Custom Route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,8 +7,18 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
-        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
+        commands: __DIR__ . '/../routes/console.php',
+        using: function () {
+
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+
+            // Group for admin routes with the admin guard
+            Route::middleware(['web', 'auth:admin'])
+                ->group(base_path('routes/admin.php'));
+
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,22 +21,3 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__ . '/auth.php';
-
-Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
-Route::post('/admin/login', [AdminController::class, 'AdminLoginSubmit'])->name('admin.login.submit');
-Route::post('/admin/login_submit', [AdminController::class, 'AdminLoginSubmit'])->name('admin.login_submit');
-Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
-Route::get('/admin/forget_password', [AdminController::class, 'AdminForgetPassword'])->name('admin.forget_password');
-
-Route::post('/admin/password_submit', [AdminController::class, 'AdminPasswordSubmit'])->name('admin.password_submit');
-
-Route::get('/admin/reset-password/{token}/{email}', [AdminController::class, 'AdminResetPassword']);
-Route::post('/admin/reset_password_submit', [AdminController::class, 'AdminResetPasswordSubmit'])->name('admin.reset_password_submit');
-
-Route::middleware('admin')->group(function () {
-    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
-    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
-    Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
-    Route::post('/admin/password/update', [AdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
-});
